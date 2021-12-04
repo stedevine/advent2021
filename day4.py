5fbc07ff148c1281e1ad0c001830bef699aed4ca
Ste Devine <stedevine@dungeon.lan>
2021-12-04 12:15:26 -0800

    updates to day4

--- day4.py
+++ day4.py
@@ -43,6 +43,40 @@ def check_grids(grids):
             if total == -5:
                 return g
 
+def updated_grids(grids, number, winning_grids):
+
+    for i in range(0, len(grids)):
+        # winning_grids holds a tuple, index of the winning board
+        # and its score.
+        # Once the grid has won - don't recalculate the score (it will change - as the game goes on the grids are updated and their final score will change)
+        if len(list(filter(lambda a:a[0] == i,winning_grids))) == 0:
+            g = grids[i]
+            row_match = False
+            col_match = False
+
+            # check rows
+            for r in range(0,5):
+                if (sum(g[r]) == -5):
+                    row_match = True
+                    continue
+            # check columns
+            for c in range(0,5):
+                total = 0
+                for r in range(0,5):
+                    total += g[r][c]
+                if total == -5:
+                    col_match = True
+                    continue
+
+            # This is the first time this grid has won
+            # put it on the list with its score.
+            if (row_match or col_match):
+                winning_grids.append((i,score(grids[i],number)))
+
+    return winning_grids
+
+
+
 def score(grid, number):
     total = 0
     for r in range(0,5):
@@ -61,7 +95,22 @@ def get_winning_board_score(data):
         if g != None:
             return score(g,n)
 
-with open('day4-test.txt') as f:
+def get_last_board(data):
+    numbers, grids = get_input(data)
+    # Track every grid that wins and the score
+    # it has when it wins
+    winning_grids = []
+    for n in numbers:
+        #print(n)
+        apply_number(n, grids)
+        updated_grids(grids, n, winning_grids)
+
+    # Game is over, what was the score on the last board that won?
+    return winning_grids[-1][1]
+
+
+with open('day4.txt') as f:
     data = f.readlines()
     data = list(map(lambda d: d.strip(), data))
     print(get_winning_board_score(data))
+    print(get_last_board(data))
